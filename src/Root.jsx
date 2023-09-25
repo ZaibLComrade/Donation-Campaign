71a389a2689bcfef66fde0ea3e61666b6e8901aa
ZaibLComrade <kmjahanzaib@gmail.com>
2023-09-25 19:47:43 +0600

    Created json fake data

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,23 +4,32 @@ import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 export default function Root() {
 	const location = useLocation();
-	console.log(location);
+	const isHome = location.pathname === "/";
 	return <div className="">
 		{/* Background Image */}
-		{location.pathname === "/" && 
-		<div className="absolute top-0 z-0 h-full">
-			<img src={ banner } className="object-cover opacity-5 w-screen h-[600px]" alt=""/>
-			<div className="absolute w-full text-center top-[37%] left-1/2 -translate-x-1/2 -translate-y-1/2">
-				<h1 className="text-5xl font-bold">I grow by helping people in need</h1>
-				<form className="mx-auto mt-10 rounded-lg w-max">
-					<input type="search" name="search" placeholder="Search here..." className="py-4 pl-4 pr-[340px] rounded-l-lg border-2 border-r-transparent"/>
-					<input type="submit" value="Search" className="py-4 font-semibold text-white border-2 rounded-r-lg cursor-pointer px-7 bg-custom-red hover:bg-transparent hover:text-custom-red border-custom-red"/>
-				</form>
+		{isHome && 
+		<div>
+			<div className="">
+				<div className="relative top-0 z-0 h-full">
+					<img src={ banner } className="object-cover opacity-5 w-screen h-[600px]" alt=""/>
+					<div className="absolute w-full text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+						<h1 className="text-5xl font-bold">I grow by helping people in need</h1>
+						<form className="mx-auto mt-10 rounded-lg w-max">
+							<input type="search" name="search" placeholder="Search here..." className="py-4 pl-4 pr-[340px] rounded-l-lg border-2 border-r-transparent"/>
+							<input type="submit" value="Search" className="py-4 font-semibold text-white border-2 rounded-r-lg cursor-pointer px-7 bg-custom-red hover:bg-transparent hover:text-custom-red border-custom-red"/>
+						</form>
+					</div>
+				</div>
 			</div>
-		</div>}
+			
+			<div>
+				Hello World
+			</div>
+		</div>
+		}
 		
 		{/* Navbar */}
-		<div className="container relative z-10 flex items-center justify-between py-12 mx-auto ">
+		<div className="container absolute top-0 z-10 flex items-center justify-between py-12 mx-auto left-1/2 -translate-x-1/2 ">
 			<div>
 				<img src={ logo } alt=""/>
 			</div>
